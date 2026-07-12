038050b63cf6dc00ea535c637c5b6d51182bb115
jackb <jackb@WIN-TOH4A3L6TPC>
2026-07-12 10:16:14 +0800

    add service health check interface

---
+++ SchedulerJobCenter/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchedulerJobCenter.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+
+    }
+}
